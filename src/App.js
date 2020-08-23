b4293a7c9712690bc21b6f826fadc7615489870b
aungureanu614 <aungureanu614@gmail.com>
2020-08-23 15:53:15 -0700

    setup complete

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div>hi book worm</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
